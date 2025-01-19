5aa9e2b589b7bd6ca7a7b9fb8e0a14e31d232168
namprice227 <e1249226@u.nus.edu>
2025-01-20 01:39:06 +0800

    Create chatbot Charlie

---
+++ src/main/java/Charlie.java
@@ -0,0 +1,7 @@
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Charlie");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you soon!");
+    }
+}
