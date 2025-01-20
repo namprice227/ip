cf82e89465fb07909013351ceafdd2e31de0c7a1
namprice227 <e1249226@u.nus.edu>
2025-01-20 12:17:58 +0800

    Save changes before switching branches

--- src/main/java/Charlie.java
+++ src/main/java/Charlie.java
@@ -1,7 +1,28 @@
-public class Duke {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Charlie {
+
     public static void main(String[] args) {
+        ArrayList<Task> tasks = new ArrayList<Task>();
         System.out.println("Hello! I'm Charlie");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you soon!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ". " + tasks.get(i).toString());
+                }
+            }
+            else if (input.equals("bye")) {
+                break;
+            } else {
+                tasks.add(new Task(input));
+                System.out.println("added: " + input);
+            }
+
+        }
+        System.out.println("Goodbye! Hope to see you soon!");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private final String activity;
+    Task(String activity) {
+        this.activity = activity;
+    }
+    public String toString() {
+        return activity;
+    }
 }
