857f60da7128ee0db1389489be43d90886eac131
namprice227 <e1249226@u.nus.edu>
2025-01-21 01:27:30 +0800

    Update with mark feature

--- src/main/java/Charlie.java
+++ src/main/java/Charlie.java
@@ -10,18 +10,37 @@ public class Charlie {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            String[] words = input.split("\\s+");
             if (input.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(i + 1 + ". " + tasks.get(i).toString());
                 }
             }
+
             else if (input.equals("bye")) {
                 break;
-            } else {
-                tasks.add(new Task(input));
-                System.out.println("added: " + input);
             }
-
+            else {
+                if (words.length == 2) {
+                    if (words[0].equals("mark")) {
+                        int index = Integer.parseInt(words[1]) - 1;
+                        tasks.get(index).mark();
+                        System.out.println("Nice, I have marked your task:");
+                        System.out.println(tasks.get(index).toString());
+                    } else if (words[0].equals("unmark")) {
+                        int index = Integer.parseInt(words[1]) - 1;
+                        tasks.get(index).unmark();
+                        System.out.println("I have unmarked your task:");
+                        System.out.println(tasks.get(index).toString());
+                    } else {
+                        tasks.add(new Task(input));
+                        System.out.println("added: " + input);
+                    }
+                } else {
+                    tasks.add(new Task(input));
+                    System.out.println("added: " + input);
+                }
+            }
         }
         System.out.println("Goodbye! Hope to see you soon!");
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,24 @@
 public class Task {
     private final String activity;
+    private Boolean marked;
     Task(String activity) {
         this.activity = activity;
+        this.marked = false;
     }
+    public void mark() {
+        this.marked = true;
+    }
+
+    public void unmark() {
+        this.marked = false;
+    }
+
     public String toString() {
-        return activity;
+        if (marked) {
+            return "[x] " + activity;
+        }
+        else {
+            return "[ ] " + activity;
+        }
     }
 }
