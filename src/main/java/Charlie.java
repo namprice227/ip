3114ea67cf133b08f33af8a0a3769e11124936ba
namprice227 <e1249226@u.nus.edu>
2025-01-21 11:52:20 +0800

    Add some handler and instructions to help it more usable

--- src/main/java/Charlie.java
+++ src/main/java/Charlie.java
@@ -10,6 +10,9 @@ public class Charlie {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                System.out.println("Please tell me what to do");
+            }
             String[] words = input.split(" ", 2);
             if (input.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
@@ -23,42 +26,49 @@ public class Charlie {
             else {
                 switch (words[0]) {
                     case "mark" -> {
-                        int index = Integer.parseInt(words[1]) - 1;
-                        tasks.get(index).mark();
-                        System.out.println("Nice, I have marked your task:");
-                        System.out.println(tasks.get(index).toString());
+                        if (CharlieHandler.isValidMark(words)) {
+                            int index = Integer.parseInt(words[1]) - 1;
+                            tasks.get(index).mark();
+                            System.out.println("Nice, I have marked your task:");
+                            System.out.println(tasks.get(index).toString());
+                        }
                     }
                     case "unmark" -> {
-                        int index = Integer.parseInt(words[1]) - 1;
-                        tasks.get(index).unmark();
-                        System.out.println("I have unmarked your task:");
-                        System.out.println(tasks.get(index).toString());
+                        if (CharlieHandler.isValidMark(words)) {
+                            int index = Integer.parseInt(words[1]) - 1;
+                            tasks.get(index).unmark();
+                            System.out.println("I have unmarked your task:");
+                            System.out.println(tasks.get(index).toString());
+                        }
                     }
                     case "todo" -> {
-                        Task task = new Todo(words[1]);
-                        tasks.add(task);
-                        System.out.println("Got it, I have added the task:");
-                        System.out.println(task);
-                        System.out.println("Now, you have " + tasks.size() + " tasks");
+                        if (CharlieHandler.isValidTodo(words)) {
+                            Task task = new Todo(words[1]);
+                            tasks.add(task);
+                            System.out.println("Got it, I have added the task:");
+                            System.out.println(task);
+                            System.out.println("Now, you have " + tasks.size() + " tasks");
+                        }
                     }
                     case "deadline" -> {
-                        Task task = new Deadline(words[1]);
-                        tasks.add(task);
-                        System.out.println("Got it, I have added the task:");
-                        System.out.println(task);
-                        System.out.println("Now, you have " + tasks.size() + " tasks");
+                        if (CharlieHandler.isValidDeadline(words)) {
+                            Task task = new Deadline(words[1]);
+                            tasks.add(task);
+                            System.out.println("Got it, I have added the task:");
+                            System.out.println(task);
+                            System.out.println("Now, you have " + tasks.size() + " tasks");
+                        }
                     }
                     case "event" -> {
-                        Task task = new Event(words[1]);
-                        tasks.add(task);
-                        System.out.println("Got it, I have added the task:");
-                        System.out.println(task);
-                        System.out.println("Now, you have " + tasks.size() + " tasks");
+                        if (CharlieHandler.isValidEvent(words)) {
+                            Task task = new Event(words[1]);
+                            tasks.add(task);
+                            System.out.println("Got it, I have added the task:");
+                            System.out.println(task);
+                            System.out.println("Now, you have " + tasks.size() + " tasks");
+                        }
                     }
-                    default -> {
-                        tasks.add(new Task(input));
-                        System.out.println("added: " + input);
-                    }
+                    default -> CharlieHandler.printHelp();
                 }
             }
         }
---
+++ src/main/java/CharlieHandler.java
@@ -0,0 +1,47 @@
+public class CharlieHandler {
+    public static boolean isValidTodo(String[] words) {
+        if (words.length < 2 || words[1].isEmpty()) {
+            System.out.println("Oh no! The description of a todo cannot be empty.");
+            System.out.println("Usage: todo <task description>");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidDeadline(String[] words) {
+        if (words.length < 2 || words[1].isEmpty()) {
+            System.out.println("Oh no! The description of a deadline cannot be empty.");
+            System.out.println("Usage: deadline <task description> /by <time>");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidEvent(String[] words) {
+        if (words.length < 2 || words[1].isEmpty()) {
+            System.out.println("Oh no! The description of an event cannot be empty.");
+            System.out.println("Usage: event <event description> /from <start time> /to <end time>");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidMark(String[] words) {
+        try {
+            Integer.parseInt(words[1]);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Oh no! The task number should be a valid integer.");
+            return false;
+        }
+    }
+
+    public static void printHelp() {
+        System.out.println("Here are the commands you can use:");
+        System.out.println("1. todo <task description>");
+        System.out.println("2. deadline <task description> /by <time>");
+        System.out.println("3. event <event description> /from <start time> /to <end time>");
+        System.out.println("4. list");
+        System.out.println("5. bye");
+    }
+}
