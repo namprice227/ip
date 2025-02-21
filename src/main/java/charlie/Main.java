cb770eaba897d73c5a0233fe38fbe0d5cfe4485a
namprice227 <e1249226@u.nus.edu>
2025-02-22 01:07:03 +0800

    Add jar header

--- src/main/java/charlie/Main.java
+++ src/main/java/charlie/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Charlie Task manager");
             fxmlLoader.<MainWindow>getController().setCharlie(charlie);
             stage.show();
         } catch (IOException e) {
